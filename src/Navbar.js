85037895efa3d0f4f7631b9c47e7fe6ea4e2e66f
cactiKinda <m.wawrzyniaczek@gmail.com>
2024-09-30 22:48:42 +0200

    initial commit

---
+++ src/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <div className="navbar-container" id="navbar-name-container">
+            <p className="navbar-name">Mateusz Wawrzyniak</p>
+        </div>
+
+        <div className="navbar-container" id="navbar-options-container">
+          <div className="navbar-option">
+            <a href="" className="navbar-option-text">Home</a>
+            <div className="navbar-option-border">&nbsp;</div>
+          </div>
+          <div className="navbar-option">
+            <a href="" className="navbar-option-text">Projects</a>
+            <div className="navbar-option-border">&nbsp;</div>
+          </div>
+          <div className="navbar-option">
+            <a href="" className="navbar-option-text">CV and contact</a>
+            <div className="navbar-option-border">&nbsp;</div>
+          </div>
+        </div>
+    </div>
+  )
+}
